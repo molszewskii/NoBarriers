6175c60f62a01a981750aa93a61874b75eb180d1
Michał <80591@student.pb.edu.pl>
2024-09-15 14:52:31 +0200

    Add LevelData and LevelDataGrammar models
    Implement LevelData class with properties for Id, Word, Translation, Sentence, SentenceTranslation, LevelId, and LanguageId, including relationships with Level and Language. Implement LevelDataGrammar class with properties for Id, Theory, Rule, Example, Exercise, ExerciseSolution, ExerciseType, LevelId, and LanguageId, including relationships with Level and Language.

---
+++ Backend/back-nobarriers/Models/Levels/LevelData.cs
@@ -0,0 +1,42 @@
+﻿using back_nobarriers.Models.Languages;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace back_nobarriers.Models.Levels
+{
+    public class LevelData
+    {
+        public int Id { get; set; }
+        public string Word { get; set; }
+        public string Translation { get; set; }
+        public string Sentence { get; set; }
+        public string SentenceTranslation { get; set; }
+        public int LevelId { get; set; }
+        [JsonIgnore]
+        public Level ? Level { get; set; }
+        public int LanguageId { get; set; }
+        [ForeignKey("LanguageId")]
+        [JsonIgnore]
+        public Language ? Language { get; set; }
+    }
+
+    public class LevelDataGrammar
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Theory { get; set; }
+        public string Rule { get; set; }
+        public string Example { get; set; } 
+        public string Exercise { get; set; } 
+        public string ExerciseSolution { get; set; } 
+        public string ExerciseType { get; set; }
+        public int LevelId { get; set; } 
+        [JsonIgnore]
+        public virtual Level ? Level { get; set; } 
+        public int LanguageId { get; set; }
+        [ForeignKey("LanguageId")]
+        [JsonIgnore]
+        public virtual Language ? Language { get; set; }
+    }
+}
