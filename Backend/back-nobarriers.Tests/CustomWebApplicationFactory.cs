353415b08508380288114d7b9f75038055a01138
Michał <80591@student.pb.edu.pl>
2024-09-15 15:21:41 +0200

    Add CustomWebApplicationFactory for testing
    Provides a custom implementation of WebApplicationFactory to use an in-memory database for integration testing.

---
+++ Backend/back-nobarriers.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,33 @@
+﻿using back_nobarriers.Models.DbContexts;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace back_nobarriers.Tests
+{
+    public class CustomWebApplicationFactory<Program> : WebApplicationFactory<Program> where Program : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<LevelDbContext>));
+                if (descriptor != null)
+                {
+                    services.Remove(descriptor);
+                }
+
+                services.AddDbContext<LevelDbContext>(options =>
+                    options.UseInMemoryDatabase("LevelDbForTesting"));
+            });
+        }
+    }
+
+
+}
