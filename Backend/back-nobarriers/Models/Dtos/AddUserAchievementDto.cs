18dc5fbc7e4a20dbe91ca630da0c4f48b50c40cc
Michał <80591@student.pb.edu.pl>
2024-09-15 14:49:37 +0200

    Add DTOs for User Achievement, Password Change, and FlashCard Box
    Implement AddUserAchievementDto with properties for Name and MaxValue. Implement ChangePasswordDto with properties for CurrentPassword and NewPassword, including validation attributes. Implement FlashCardBoxDto with properties for Title, Author, UserId, FlashCards list, CategoryId, and LanguageId.

---
+++ Backend/back-nobarriers/Models/Dtos/AddUserAchievementDto.cs
@@ -0,0 +1,9 @@
+﻿namespace back_nobarriers.Models.Dtos
+{
+    public class AddUserAchievementDto
+    {
+        public string Name { get; set; }
+        public int MaxValue { get; set; }
+
+    }
+}
---
+++ Backend/back-nobarriers/Models/Dtos/FlashCardBoxDto.cs
@@ -0,0 +1,16 @@
+﻿using back_nobarriers.Models.FlashCards;
+
+namespace back_nobarriers.Models.Dtos
+{
+    public class FlashCardBoxDto
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        public string UserId { get; set; }
+
+        public List<FlashCard> FlashCards { get; set; }
+        public int CategoryId { get; set; }
+        public int LanguageId { get; set; }
+    }
+}
