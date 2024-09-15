0dea9ad377e292dc160b39ecb1b14d2640533680
Michał <80591@student.pb.edu.pl>
2024-09-15 14:50:06 +0200

    Add File model
    Implement File class with properties for Id, FileName, FilePath, UploadDate, and UserId. Define UserId as a foreign key with a relationship to User.

---
+++ Backend/back-nobarriers/Models/Files/File.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace back_nobarriers.Models.Files
+{
+    public class File
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public DateTime UploadDate { get; set; }
+
+        public string UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
