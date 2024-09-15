217f8808e8f6e2323536bf6a692922ae4aff7e9b
Michał <80591@student.pb.edu.pl>
2024-09-15 14:51:24 +0200

    Add Comment and ForumQuestion models
    Implement Comment class with properties for Id, Description, AuthorId, AuthorName, ForumQuestionId, Timestamp, and relationships with User and ForumQuestion. Implement ForumQuestion class with properties for Id, Title, Description, AuthorId, AuthorName, Timestamp, and a list of Comments.

---
+++ Backend/back-nobarriers/Models/Forum/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace back_nobarriers.Models.Forum
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Descritpion { get; set; }
+        public string AuthorId { get; set; }
+        [ForeignKey("AuthorId")]
+        [JsonIgnore]
+        public User? User { get; set; }
+        public string ? AuthorName { get; set; } = string.Empty;
+        public int ForumQuestionId { get; set; }
+        public DateTime Timestamp { get; set; }
+        [JsonIgnore]
+        public ForumQuestion ? ForumQuestion { get; set; }
+    }
+}
---
+++ Backend/back-nobarriers/Models/Forum/ForumQuestion.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace back_nobarriers.Models.Forum
+{
+    public class ForumQuestion
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string AuthorId { get; set; }
+        [ForeignKey("AuthorId")]
+        [JsonIgnore]
+        public User? User { get; set; }
+        public string ? AuthorName { get; set; } = string.Empty;
+        public DateTime Timestamp { get; set; }
+        public List<Comment> ? CommentsList { get; set; }
+
+    }
+}
