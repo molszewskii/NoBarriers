667df8ee8ba5b2b26d3855baaf1c448935b228e6
Michał <80591@student.pb.edu.pl>
2024-09-15 14:44:02 +0200

    Add UserAchievements and Achievement models
    Implement UserAchievements model with properties for Id, Progress, AchievementId, and UserId. Define relationships with Achievement and User entities. Implement Achievement model with properties for Id, Name, MaxValue, and a list of UserAchievements. Added JSON serialization settings with [JsonIgnore] attributes.

---
+++ Backend/back-nobarriers/Models/Achievements/UserAchievement.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace back_nobarriers.Models.UserAchievement
+{
+
+    public class UserAchievements
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Progress { get; set; }
+
+        public int AchievementId { get; set; }
+        [JsonIgnore]
+        public Achievement Achievement { get; set; }
+
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+        [JsonIgnore]
+        public User? User { get; set; }
+    }
+
+    public class Achievement
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int MaxValue { get; set; }
+        public List<UserAchievements> ? UserAchievements { get; set; }
+    }
+
+
+}
