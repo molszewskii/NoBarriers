f5aa36a93af80ae95cd8246b44d248d15ab81ee9
Michał <80591@student.pb.edu.pl>
2024-09-15 14:37:15 +0200

    Add User model

---
+++ Backend/back-nobarriers/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace back_nobarriers.Models
+{
+    public class User : IdentityUser
+    {
+        public string Surname { get; set; }
+        public string Role { get; set; }
+
+        public string LanguageCourse { get; set; }
+    }
+}
