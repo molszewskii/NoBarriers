fda5b55def903d2fe2083350de2374ec409f9051
Michał <80591@student.pb.edu.pl>
2024-09-15 14:55:51 +0200

    Add UserDto and UserRoles
    Implement UserDto class with properties for Name, Surname, Email, Password, and Role. Implement UserRoles class with constant values for Admin, User, and Teacher roles.

---
+++ Backend/back-nobarriers/Models/UserDto.cs
@@ -0,0 +1,14 @@
+﻿namespace back_nobarriers.Models
+{
+    public class UserDto
+    {   
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+        public  string Email { get; set; }
+        public  string Password { get; set; }
+
+        public string Role { get; set; }
+
+    }
+}
---
+++ Backend/back-nobarriers/Models/UserRoles.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Runtime.Serialization;
+
+namespace back_nobarriers.Models
+{
+    public class UserRoles
+    {
+        public const string Admin = "ADMIN";
+        public const string User = "USER";
+        public const string Teacher = "TEACHER";
+    }
+}
