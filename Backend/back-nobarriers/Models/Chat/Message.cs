82f899a4bdc3c312b5ec2a556bc76f95a6cc4ea5
Michał <80591@student.pb.edu.pl>
2024-09-15 14:48:13 +0200

    Add Message model
    Implement Message class with properties for Id, SenderId, RecipientId, Content, and Timestamp. Added support for nullable SenderId and RecipientId.

---
+++ Backend/back-nobarriers/Models/Chat/Message.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace back_nobarriers.Models.Chat
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string ? SenderId { get; set; }
+        public string ? RecipientId { get; set; }
+        public string Content { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
