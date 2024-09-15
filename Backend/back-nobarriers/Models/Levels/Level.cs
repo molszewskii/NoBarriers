63440707bd5e63216250d6e636d1caf8e1b39815
Michał <80591@student.pb.edu.pl>
2024-09-15 14:52:04 +0200

    Add Level model
    Implement Level class with properties for Id, Name, Description, CategoryId, and a relationship with Category.

---
+++ Backend/back-nobarriers/Models/Levels/Level.cs
@@ -0,0 +1,17 @@
+﻿using back_nobarriers.Models.Categories;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace back_nobarriers.Models.Levels
+{
+    public class Level
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; } 
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        [JsonIgnore]
+        public Category ? Category { get; set; }
+    }
+}
