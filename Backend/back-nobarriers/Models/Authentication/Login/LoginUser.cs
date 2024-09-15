2b0e8f75bcf44f13146254cf061ca276b96ae8cd
Michał <80591@student.pb.edu.pl>
2024-09-15 14:47:06 +0200

    Add LoginUser and RegisterUser models
    Implement LoginUser class with properties for Email and Password, including validation attributes. Implement RegisterUser class with properties for Name, Surname, Email, Password, Role, and LanguageCourse.

---
+++ Backend/back-nobarriers/Models/Authentication/Login/LoginUser.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace back_nobarriers.Models.Authentication.Login
+{
+    public class LoginUser
+    {
+        [EmailAddress]
+        [Required(ErrorMessage ="Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="Password is required")]
+        public string Password { get; set; }
+    }
+}
---
+++ Backend/back-nobarriers/Models/Authentication/Register/RegisterUser.cs
@@ -0,0 +1,15 @@
+﻿namespace back_nobarriers.Models.Authentication.Register
+{
+    public class RegisterUser
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public string Role { get; set; }
+
+        public string LanguageCourse { get; set; }
+    }
+}
