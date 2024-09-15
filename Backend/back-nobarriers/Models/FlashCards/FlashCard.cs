c9b053db346fbcaf4872ab8adfbfc517414f3584
Michał <80591@student.pb.edu.pl>
2024-09-15 14:50:46 +0200

    Add FlashCard, FlashCardBox, and LikedBox models
    Implement FlashCard class with properties for Id, Term, TermTranslation, and FlashCardBoxId. Implement FlashCardBox class with properties for Id, Title, NumberOfTerms, Author, UserId, CategoryId, and LanguageId, including relationships with User, Category, and Language. Implement LikedBox class with properties for Id, UserId, BoxId, IsIconClicked, and a relationship with FlashCardBox.

---
+++ Backend/back-nobarriers/Models/FlashCards/FlashCard.cs
@@ -0,0 +1,52 @@
+﻿using back_nobarriers.Models.Categories;
+using back_nobarriers.Models.Languages;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace back_nobarriers.Models.FlashCards
+{
+    public class FlashCard
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Term { get; set; }
+        public string TermTranslation { get; set; }
+
+        public int FlashCardBoxId { get; set; }
+
+    }
+
+    public class FlashCardBox
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int NumberOfTerms { get; set; }
+        public string Author { get; set; }
+        public string UserId { get; set; }
+        [ForeignKey("UserId")]
+        [JsonIgnore]
+        public User? User { get; set; }
+        public List<FlashCard> Cards { get; set; }
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category ? Category { get; set; }
+        public int LanguageId { get; set; }
+        [ForeignKey("LanguageId")]
+        public Language Language { get; set; }
+
+    }
+
+    public class LikedBox
+    {
+        [Key]
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public int BoxId { get; set; }
+        public bool IsIconClicked { get; set; }
+        [ForeignKey("BoxId")]
+        public FlashCardBox Box { get; set; }
+       
+    }
+}
