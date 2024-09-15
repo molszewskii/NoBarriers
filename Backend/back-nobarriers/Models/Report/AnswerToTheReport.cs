95d36fe2f63d4acd8a30da02845e25613c556112
Michał <80591@student.pb.edu.pl>
2024-09-15 14:53:56 +0200

    Add AnswerToTheReport and ProblemReport models
    Implement AnswerToTheReport class with properties for Id, SenderId, ReceiverId, AnswerText, ReportId, AnswerDate, and a relationship with ProblemReport. Implement ProblemReport class with properties for Id, UserId, Description, ReportDate, RecipientId, TestId, and a relationship with Test.

---
+++ Backend/back-nobarriers/Models/Report/AnswerToTheReport.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace back_nobarriers.Models.Report
+{
+    public class AnswerToTheReport
+    {
+        [Key]
+        public int Id { get; set; }
+        public string SenderId { get; set; }
+        public string ReceiverId { get; set; }
+        public string AnswerText { get; set; }
+        public int ReportId { get; set; }
+        public DateTime AnswerDate { get; set; } = DateTime.UtcNow;
+        [ForeignKey("ReportId")]
+        [JsonIgnore]
+        public ProblemReport ? Report { get; set; }
+
+    }
+}
---
+++ Backend/back-nobarriers/Models/Report/ProblemReport.cs
@@ -0,0 +1,22 @@
+﻿using back_nobarriers.Models.Tests;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace back_nobarriers.Models.Report
+{
+    public class ProblemReport
+    {
+        [Key]
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Description { get; set; }
+        public DateTime ReportDate { get; set; } = DateTime.UtcNow;
+        public string RecipientId { get; set; }
+        
+        public int TestId { get; set; }
+        [ForeignKey("TestId")]
+        [JsonIgnore]
+        public Test ? Test { get; set; }
+    }
+}
