e0a1a68830c365db38ec17983de76306090baa83
Michał <80591@student.pb.edu.pl>
2024-09-15 15:11:45 +0200

    Add LanguageController
    Implement CRUD operations for managing languages, including retrieving all languages, retrieving by ID, creating, updating, and deleting languages.

---
+++ Backend/back-nobarriers/Controllers/LanguageController.cs
@@ -0,0 +1,98 @@
+﻿using back_nobarriers.Models.DbContexts;
+using back_nobarriers.Models.Languages;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace back_nobarriers.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LanguageController : ControllerBase
+    {
+        private readonly LanguageDbContext _context;
+
+        public LanguageController(LanguageDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Language>>> GetLanguages()
+        {
+            return await _context.Languages.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Language>> GetLanguage(int id)
+        {
+            var language = await _context.Languages.FindAsync(id);
+
+            if (language == null)
+            {
+                return NotFound();
+            }
+
+            return language;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Language>> PostLanguage(Language language)
+        {
+            _context.Languages.Add(language);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetLanguage), new { id = language.Id }, language);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLanguage(int id, Language language)
+        {
+            if (id != language.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(language).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LanguageExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLanguage(int id)
+        {
+            var language = await _context.Languages.FindAsync(id);
+
+            if (language == null)
+            {
+                return NotFound();
+            }
+
+            _context.Languages.Remove(language);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool LanguageExists(int id)
+        {
+            return _context.Languages.Any(e => e.Id == id);
+        }
+    }
+}
