a96bf0c64188e613c9db76b8657ed5c1cbef7b15
Michał <80591@student.pb.edu.pl>
2024-09-15 14:51:40 +0200

    Add Language model
    Implement Language class with properties for Id and Name.

---
+++ Backend/back-nobarriers/Models/Languages/Language.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace back_nobarriers.Models.Languages
+{
+    public class Language
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+    }
+}
