a6e15e229e8fe057073009214603afc07600a332
Michał <80591@student.pb.edu.pl>
2024-09-15 14:52:48 +0200

    Add UserLevelProgress model
    Implement UserLevelProgress class with properties for Id, UserId, LevelId, IsCompleted, and a relationship with Level.

---
+++ Backend/back-nobarriers/Models/Levels/UserLevelProgress.cs
@@ -0,0 +1,12 @@
+﻿namespace back_nobarriers.Models.Levels
+{
+    public class UserLevelProgress
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public int LevelId { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public Level Level { get; set; }
+    }
+}
